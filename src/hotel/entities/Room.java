8fae40e63253fe70aaf6abd99a0f157e01a33fe6
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-09-13 23:24:57 +1000

    Remove unnecessary spaces By Gurpreet Gill

--- src/hotel/entities/Room.java
+++ src/hotel/entities/Room.java
@@ -3,19 +3,15 @@ package hotel.entities;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import hotel.credit.CreditCard;
 import hotel.utils.IOUtils;
 
 public class Room {
-	
-	private enum State {READY, OCCUPIED}
-	
+	private enum State {READY, OCCUPIED}	
 	int id;
 	RoomType roomType;
 	List<Booking> bookings;
 	State state;
-
 	
 	public Room(int id, RoomType roomType) {
 		this.id = id;
@@ -23,26 +19,18 @@ public class Room {
 		bookings = new ArrayList<>();
 		state = State.READY;
 	}
-	
-
 	public String toString() {
 		return String.format("Room : %d, %s", id, roomType);
 	}
-
-
 	public int getId() {
 		return id;
-	}
-	
+	}	
 	public String getDescription() {
 		return roomType.getDescription();
 	}
-	
-	
 	public RoomType getType() {
 		return roomType;
 	}
-	
 	public boolean isAvailable(Date arrivalDate, int stayLength) {
 		IOUtils.trace("Room: isAvailable");
 		for (Booking b : bookings) {
@@ -51,9 +39,7 @@ public class Room {
 			}
 		}
 		return true;
-	}
-	
-	
+	}	
 	public boolean isReady() {
 		return state == State.READY;
 	}
@@ -64,7 +50,6 @@ public class Room {
 		bookings.add(booking);// insert data into dataset.
 		return booking; // return the dataset.
 	}
-
 	// This method is implemented by Gurpreet Gill
 	public void checkin() {
 		// Check if ready state is ready or not?
@@ -75,11 +60,7 @@ public class Room {
 			throw new RuntimeException("Room state is already occupied!");//show a runtime error message if the room state is already in occupied state.
 		}
 	}
-
-
 	public void checkout(Booking booking) {
 		
 	}
-
-
 }
