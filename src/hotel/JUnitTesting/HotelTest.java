6a568ec7fae6cc237444991ea56688a693829d8e
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-09-14 20:25:23 +1000

    Imported required libraries for testing

---
+++ src/hotel/JUnitTesting/HotelTest.java
@@ -0,0 +1,9 @@
+import hotel.credit.CreditCard;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import hotel.credit.CreditCardType;
