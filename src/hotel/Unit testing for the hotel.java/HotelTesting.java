d7dbaf9d86ccd5a9953cb12f5c4707f0f1985711
tgcsu515 <aman_thandi87@yahoo.com>
2018-09-18 19:04:32 +1000

    import the libraray files for Testing

---
+++ src/hotel/Unit testing for the hotel.java/HotelTesting.java
@@ -0,0 +1,11 @@
+
+// import the library file
+import hotel.credit.CreditCard;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import hotel.credit.CreditCardType;
