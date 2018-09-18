4cb1358877a6990cfd94cfb1e0e0308bdcfa9d67
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-09-18 13:49:03 -0700

    Added test method to test the Record Service UI state after calling the serviceDetailsEntered() method

--- src/hotel/test/Integration Testing/RecordServiceCTLTest.java
+++ src/hotel/test/Integration Testing/RecordServiceCTLTest.java
@@ -36,6 +36,17 @@ public class RecordServiceCTLTest {
         String actualRecordServiceState = recordServiceCTLObj.getState(); //Get the current state of booking
         assertEquals(expectedRecordServiceState, actualRecordServiceState); //Compare the  expected result with the actual result
     }
+	
+	/*Test Record Service UI State after calling the serviceDetailsEntered method of RecordServiceCTL class*/
+    @Test
+    public void testRecordServiceUIStateChangeOfServiceDetailsEntered() {
+        String expectedRecordServiceUIstate = "COMPLETED"; //Define the expected result
+        recordServiceCTLObj.setState();
+        recordServiceCTLObj.serviceDetailsEntered(ServiceType.ROOM_SERVICE, 200);//Call the method
+        String actualRecordServiceUIState = recordServiceCTLObj.getRecordServiceUIState(); //Get the current state of RecordServiceUI
+        assertEquals(expectedRecordServiceUIstate, actualRecordServiceUIState); //Compare the  expected result with the actual result
+
+    }
 
 
 
