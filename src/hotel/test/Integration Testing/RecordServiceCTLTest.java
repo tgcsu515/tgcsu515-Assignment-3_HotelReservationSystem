988e604254ad0cbca9f9b4c5f7451ff9f0b85e68
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-09-18 13:42:50 -0700

    Create RecordServiceCTLTest class inside the "Integration Testing" folder

---
+++ src/hotel/test/Integration Testing/RecordServiceCTLTest.java
@@ -0,0 +1,14 @@
+package hotel.service;
+
+import hotel.entities.Hotel;
+import hotel.entities.ServiceType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RecordServiceCTLTest {
+
+
+
+}
