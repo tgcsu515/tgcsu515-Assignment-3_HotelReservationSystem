a17a66d04ad144d576aeca5a8116fb5761eb8a50
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-09-18 13:44:50 -0700

    Created the integration testing environment

--- src/hotel/test/Integration Testing/RecordServiceCTLTest.java
+++ src/hotel/test/Integration Testing/RecordServiceCTLTest.java
@@ -8,6 +8,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RecordServiceCTLTest {
+	
+	RecordServiceCTL recordServiceCTLObj;
+    RecordServiceUI recordServiceUIObj;
+    Hotel hotelObj;
+
+    @Before
+    public void setUp() {
+        hotelObj = new Hotel();
+        recordServiceCTLObj = new RecordServiceCTL(hotelObj);
+        recordServiceUIObj = new RecordServiceUI(recordServiceCTLObj);
+    }
+
+    @After
+    public void tearDown() {
+        hotelObj = null;
+        recordServiceCTLObj = null;
+        recordServiceUIObj = null;
+    }
 
 
 
