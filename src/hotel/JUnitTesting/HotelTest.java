78a0f85ba72f2a456078071325a8daa7b9527c00
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-09-18 13:15:05 +1000

    Added inline comments

--- src/hotel/JUnitTesting/HotelTest.java
+++ src/hotel/JUnitTesting/HotelTest.java
@@ -9,66 +9,70 @@ import static org.junit.Assert.*;
 import hotel.credit.CreditCardType;
 
 public class HotelTest {
-    Hotel hotelObj;
+    Hotel hotelObj;	//created required variable
    
     
     @Before
     public void setUp() {
-        hotelObj = new Hotel();
+        hotelObj = new Hotel();	//created object of Hotel class
     }
     
     @After
     public void tearDown() {
-        hotelObj = null;
+        hotelObj = null;		//initialize the object with "null" value
     }
 	
+	//Test of "book" method of Hotel class
 	@Test
     public void testBook(){
-        String expectedConfirmationNumber = "14820182001"; 
-        Guest newGuest = new Guest("David", "Melbourne", 041234566);
-        Room newRoom = new Room(2001, RoomType.DOUBLE);
-        Date arrivalDate = new Date();
-        int stayLength = 3;
-        int noOfOccupants = 2;
-        CreditCard newCreditCard = new CreditCard(CreditCardType.VISA, 234567788, 123);
-        Booking bookingObj = new Booking(newGuest, newRoom, arrivalDate, stayLength, noOfOccupants, newCreditCard);
-        long currentConfirmationNumber = bookingObj.getConfirmationNumber();
+        String expectedConfirmationNumber = "14820182001"; //defined expected result
+        Guest newGuest = new Guest("David", "Melbourne", 041234566); //passing values to the parameters of guest
+        Room newRoom = new Room(2001, RoomType.DOUBLE);//passing values to the parameters of room
+        Date arrivalDate = new Date();//creating object of date
+        int stayLength = 3;//defining and initialize staylength
+        int noOfOccupants = 2;//defining and initialize noOfOccupants
+        CreditCard newCreditCard = new CreditCard(CreditCardType.VISA, 234567788, 123);//passing values to the parameters of creditcard
+        Booking bookingObj = new Booking(newGuest, newRoom, arrivalDate, stayLength, noOfOccupants, newCreditCard);//Create an instance of Booking class using the above parameters
+        long currentConfirmationNumber = bookingObj.getConfirmationNumber();//calling getConfirmationNumber method
         String actualConfirmationNumber = Long.toString(currentConfirmationNumber);
-        assertEquals(expectedConfirmationNumber, actualConfirmationNumber);
+        assertEquals(expectedConfirmationNumber, actualConfirmationNumber);//comparing actual result with expected
     }
 	
+	//Test of "checkin" method of Hotel class
 	@Test
     public void testCheckinRunTimeException(){
         String actualExceptionMessage = "";
-        String expectedExceptionMessage = "Current Booking is null";
+        String expectedExceptionMessage = "Current Booking is null";//defined expected result
         try{
-            hotelObj.checkin(0);
+            hotelObj.checkin(0);//calling checkin method of hotel class
         }
         catch(Exception ex){
-            actualExceptionMessage = ex.getMessage();
+            actualExceptionMessage = ex.getMessage();//getting actual result
         }
-        assertEquals(expectedExceptionMessage, actualExceptionMessage);         
+        assertEquals(expectedExceptionMessage, actualExceptionMessage);  //comparing actual result with expected       
     }
 	
+	//Test of "checkout" method of Hotel class
 	@Test
     public void testCheckoutRunTimeException(){
         String actualExceptionMessage = "";
-        String expectedExceptionMessage = "Hotel: checkout: no booking present for room id";
+        String expectedExceptionMessage = "Hotel: checkout: no booking present for room id";//defined expected result
         try{
-            hotelObj.checkout(0);
+            hotelObj.checkout(0);//calling checkout method of hotel class
         }
         catch(Exception e){
-            actualExceptionMessage = e.getMessage();
+            actualExceptionMessage = e.getMessage();//getting actual result
         }
-        assertEquals(expectedExceptionMessage, actualExceptionMessage);
+        assertEquals(expectedExceptionMessage, actualExceptionMessage);//comparing actual result with expected
     }
 	
+	//Test of "addServiceCharge" method of Hotel class
 	@Test
     public void testAddServiceChargeRunTimeException() {
         String actualExceptionMessage = "";
         String expectedExceptionMessage = "no booking present for room id"; //Define expected result
         try {
-            hotelObj.addServiceCharge(0, ServiceType.BAR_FRIDGE, 100); //Call the method
+            hotelObj.addServiceCharge(0, ServiceType.BAR_FRIDGE, 100); //Calling addServiceCharge method of hotel class
         } catch (Exception ex) {
             actualExceptionMessage = ex.getMessage(); //Get actual result
         }
