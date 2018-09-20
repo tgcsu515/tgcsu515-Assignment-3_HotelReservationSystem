883372845a82c103608b9101819dd4fa3eaf03a8
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-09-20 21:33:10 +1000

    testCheckIn() test method Implemented By Gurpreet Gill
    Testing results is 100% Passed.

---
+++ src/hotel/test/Unit Testing/RoomTest.java
@@ -0,0 +1,51 @@
+package hotel.entities;
+
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import java.util.Date;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+public class RoomTest {
+    Booking booking;
+    Room room;
+    int id;
+    RoomType roomType;
+    Guest newGuestObj;
+    Date arrivalDate = new Date();
+    int stayLength;
+    int noOfOccupants;
+    CreditCard newCreditCardObj;
+
+    @Before
+    public void setUp() {
+        newGuestObj = new Guest("John Wick", "Sydney", 042342222);
+        room = new Room(1102, RoomType.SINGLE);
+        //Date arrivalDate = new Date();
+        stayLength = 4;
+        noOfOccupants = 1;
+        newCreditCardObj = new CreditCard(CreditCardType.VISA, 589635478, 545);
+        booking = new Booking(newGuestObj, room, arrivalDate, stayLength, noOfOccupants, newCreditCardObj);
+        room = new Room(1102, RoomType.SINGLE);
+    }
+	
+	
+    @After
+    public void tearDown() {
+        booking = null;
+    }
+	
+    /*Test of checkIn method of Booking class*/
+    @Test
+    public void testCheckIn() {
+        String expectedResult = "OCCUPIED";
+        room.checkIn(); 
+        String actualResult = room.getState();
+        assertEquals(expectedResult, actualResult);
+    }
+}
