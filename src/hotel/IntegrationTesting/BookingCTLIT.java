dea84cca890d7fd8f21b51c2d9ec9448f35d0986
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-09-22 17:33:27 +1000

    Created setup() for integration testing of "BookingCTL.java"

--- src/hotel/IntegrationTesting/BookingCTLIT.java
+++ src/hotel/IntegrationTesting/BookingCTLIT.java
@@ -13,3 +13,34 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+public class BookingCTLIT {
+    Hotel hotelObj;
+    BookingCTL bookingCTLObj;
+    BookingUI bookingUIObj;
+    Booking bookingObj;
+	
+	@Before
+    public void setUp() {
+        hotelObj = new Hotel();
+        bookingUIObj = new BookingUI(bookingCTLObj);
+        //bookingObj = new BookingObj();
+        Guest newGuest = new Guest("Peter Dawson", "Melbourne", 042345224);
+        Room newRoom = new Room(1102, RoomType.SINGLE);
+        Date arrivalDate = new Date();
+        int stayLength = 2;
+        int noOfOccupants = 1;
+        CreditCard newCreditCard = new CreditCard(CreditCardType.VISA, 234688654, 248);
+        //Create an instance of Booking class using the above parameters
+        bookingObj = new Booking(newGuest, newRoom, arrivalDate, stayLength, noOfOccupants, newCreditCard);
+        bookingCTLObj = new BookingCTL(hotelObj);
+        
+    }
+    
+    @After
+    public void tearDown() {
+        bookingCTLObj = null;
+        bookingUIObj = null;
+        bookingObj = null;
+    }
+}
