4f1ed30a69a1b43b787455009e8eff3bbec95b47
tgcsu515 <aman_thandi87@yahoo.com>
2018-09-22 22:28:00 +1000

    created the class and define the variables

--- src/hotel/Integration Testing for the BookingCTL.java/BookingCTLIT.java
+++ src/hotel/Integration Testing for the BookingCTL.java/BookingCTLIT.java
@@ -5,3 +5,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+public class BookingCTLIT {
+
+    BookingCTL currentBookingControl;
+    Hotel currentHotel;
